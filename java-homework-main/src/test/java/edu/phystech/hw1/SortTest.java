e347ad2659c3c7a87c4bf96fcdd5da6c4a058e29
MinikoCappu <96332603+MinikoCappu@users.noreply.github.com>
2024-03-23 00:13:20 +0800

    Добавлены решения

--- java-homework-main/src/test/java/edu/phystech/hw1/SortTest.java
+++ java-homework-main/src/test/java/edu/phystech/hw1/SortTest.java
@@ -8,7 +8,17 @@ import org.junit.jupiter.api.Assertions;
 public class SortTest {
 
     private static int[] sort(int[] nums) {
-        return nums;
+        int[] nums_copy = Arrays.copyOf(nums,nums.length);
+        for(int i = 0; i < nums_copy.length - 1; i++) {
+            for(int j = i; j < nums_copy.length; j++) {
+                if (nums_copy[i] > nums_copy[j]) {
+                    int tmp = nums_copy[i];
+                    nums_copy[i] = nums_copy[j];
+                    nums_copy[j] = tmp;
+                }
+            }
+        }
+        return nums_copy;
     }
 
     @Test
