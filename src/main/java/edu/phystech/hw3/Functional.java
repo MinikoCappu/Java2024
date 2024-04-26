aa7b3cdf86e2b217c17ac3b85984fdff29784044
MinikoCappu <96332603+MinikoCappu@users.noreply.github.com>
2024-04-27 01:42:08 +0800

    4 дз

--- src/main/java/edu/phystech/hw3/Functional.java
+++ src/main/java/edu/phystech/hw3/Functional.java
@@ -1,17 +1,26 @@
 package edu.phystech.hw3;
 
 import java.util.List;
+import java.util.ArrayList;
 import java.util.function.BinaryOperator;
 import java.util.function.Function;
 
 public class Functional {
 
-    public static <T, R> List<R> map(List<T> collection, Function<T, R> function) {
-        return null;
+    public static <T, R> List<R> map(List<? extends T> collection, Function<? super T, ? extends R> function) {
+        List<R> ans = new ArrayList<>();
+        for (T item : collection) {
+            ans.add(function.apply(item));
+        }
+        return ans;
     }
 
     public static <T, R> T reduce(List<T> collection, BinaryOperator<T> operator, T identity) {
-        return null;
+        T ans = identity;
+        for (T item : collection) {
+            ans = operator.apply(ans, item);
+        }
+        return ans;
     }
 }
 
--- src/main/java/edu/phystech/hw3/SetUtils.java
+++ src/main/java/edu/phystech/hw3/SetUtils.java
@@ -1,22 +1,30 @@
 package edu.phystech.hw3;
 
 import java.util.Set;
+import java.util.HashSet;
 
 public class SetUtils {
     public static <E> Set<E> union(Set<? extends E> s1, Set<? extends E> s2) {
-        return null;
+        Set<E> union = new HashSet<>(s1);
+        union.addAll(s2);
+        return union;
     }
 
     public static <E> Set<E> intersection(Set<? extends E> s1, Set<? extends E> s2) {
-        return null;
+        Set<E> intersection = new HashSet<>(s1);
+        intersection.retainAll(s2);
+        return intersection;
     }
 
     public static <E> Set<E> difference(Set<? extends E> s1, Set<? extends E> s2) {
-        return null;
+        Set<E> difference = new HashSet<>(s1);
+        difference.removeAll(s2);
+        return difference;
     }
 
     public static <E> Set<E> symmetricDifference(Set<? extends E> s1, Set<? extends E> s2) {
-        return null;
+        Set<E> symmetricDifference = new HashSet<>(union(s1, s2));
+        symmetricDifference.removeAll(intersection(s1, s2));
+        return symmetricDifference;
     }
-
 }
